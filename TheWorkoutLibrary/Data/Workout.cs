f4d76ea2f723dc535518864e99985c73b770023b
PETER MANZ <2013750@chester.ac.uk>
2022-05-05 14:20:36 +0100

    Create Workout Class in DataFolder

---
+++ TheWorkoutLibrary/Data/Workout.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace TheWorkoutLibrary.Data
+{
+    public class Workout
+    {
+        [Key]
+        public int workoutID { get; set; }
+        [StringLength(25)]
+        public string workoutName { get; set; }
+        [StringLength(100)]
+        public string workoutNotes { get; set; }
+        [StringLength(25)]
+        public string workoutDifficulty { get; set; }
+        public Boolean activityStatus { get; set; }
+        public byte[] videoData { get; set; }
+    }
+}
--- TheWorkoutLibrary/Startup.cs
+++ TheWorkoutLibrary/Startup.cs
@@ -15,6 +15,8 @@ namespace TheWorkoutLibrary
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+       
+        
         public IConfiguration Configuration { get; }
 
         public Startup(IConfiguration configuration)
