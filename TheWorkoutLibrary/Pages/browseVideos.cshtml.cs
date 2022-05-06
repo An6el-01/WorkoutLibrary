9a9f9acf0f39f2e26eb4dbc7705cbbd54aca1f7c
BuildTools <unconfigured@null.spigotmc.org>
2022-05-06 16:10:22 +0100

    Videos working

--- TheWorkoutLibrary/Pages/browseVideos.cshtml.cs
+++ TheWorkoutLibrary/Pages/browseVideos.cshtml.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TheWorkoutLibrary.Data;
+
+
 
 namespace TheWorkoutLibrary.Pages
 {
     public class browseVideosModel : PageModel
     {
+        private readonly AppDbContext _db;
+        public IList<Workout> Workout { get; private set; }
+        
+
+        public browseVideosModel(AppDbContext db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
+            Workout = _db.videoWorkoutLibrary.FromSqlRaw("SELECT * FROM videoWorkoutLibrary").ToList();
         }
     }
 }
