0fe73c586c37676b07330b8de97f8d2bca5d8503
BuildTools <unconfigured@null.spigotmc.org>
2022-05-25 10:42:00 +0100

    Calendar done

--- TheWorkoutLibrary/Pages/Calendar.cshtml.cs
+++ TheWorkoutLibrary/Pages/Calendar.cshtml.cs
@@ -2,15 +2,48 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TheWorkoutLibrary.Data;
 
 namespace TheWorkoutLibrary.Pages
 {
     public class CalendarModel : PageModel
     {
-        public void OnGet()
+
+        [BindProperty] public Workout calendarWorkout { get; set; }
+
+        public IList<WorkoutExcercise> workoutExcercisesCalendar { get; set; }
+
+        public IList<Excercise> excercisesCalendar { get; private set; }
+
+        private readonly AppDbContext _db;
+
+        private readonly UserManager<ApplicationUser> _um;
+
+        public CalendarModel(AppDbContext db, UserManager<ApplicationUser> um)
         {
+            _db = db;
+            _um = um;
         }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            excercisesCalendar = _db.Excercise.ToList();
+            workoutExcercisesCalendar = _db.WorkoutExcercise.ToList();
+            calendarWorkout = await _db.Workout.FindAsync(1);
+
+            if (calendarWorkout == null)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
+        }
+
+
     }
+
+
 }
